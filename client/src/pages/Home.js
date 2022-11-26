03dcd753398cc10e1395666ae9406518a68c3dc5
Marty <martynolan17@gmail.com>
2022-11-26 14:00:16 -0500

    added cctLogo for homepage + style for homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import cctlogo1 from "../assets/images/cctLogoLarge.jpg";
+
+// import css
+import "./homeStyle.css";
+
 
 const Home = () => {
 
     return (
-        <div>
+        <div className="main">
+            <img className="cctLogoLarge" src={cctlogo1} alt="outline of horse with parliament buildings in background"></img>
             <h1>Home</h1>
         </div>
     )
