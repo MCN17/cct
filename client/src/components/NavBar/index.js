68868dabcca261d67bd136e8a4e523e2122adcb2
Marty <martynolan17@gmail.com>
2022-11-26 09:20:49 -0500

    added dropdown menu

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -5,25 +6,60 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import cctlogo from "../../assets/images/cctlogo.png";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+// import cctlogo from "../../assets/images/cctlogo.png";
+// {/* <img className="logo" src={cctlogo} alt="outline of horse with parliament buildings in background"></img> */}
 
 // import NavBar css
 import "./NavBar.css"
 
+
+
 const NavBar = () => {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar className="nav">
-        <img className="logo" src={cctlogo} alt="outline of horse with parliament buildings in background"></img>
+
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>The Team</MenuItem>
+            <MenuItem onClick={handleClose}>Cicruits</MenuItem>
+            <MenuItem onClick={handleClose}>Top Routes</MenuItem>
+          </Menu>
+        
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
           >
-            <MenuIcon />
+          <MenuIcon />
           </IconButton>
           <Typography className="title" variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Cap City Trotters
