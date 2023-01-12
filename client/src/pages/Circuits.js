0e636acd01b69e1e1ffd36036ebc7c13b67807d4
Marty <martynolan17@gmail.com>
2023-01-12 09:52:35 -0500

    added remaining ride cards to circuits page

--- client/src/pages/Circuits.js
+++ client/src/pages/Circuits.js
@@ -1,5 +1,11 @@
 import React from 'react';
+
+// import strava pics
 import lordsEye from "../assets/images/lordsEye.jpg";
+import endoz from "../assets/images/endoz.jpg";
+import kennysLoop from "../assets/images/kennysLoop.jpg";
+import champlainLookout from "../assets/images/champlainLookout.jpg";
+
 
 
 // Import Post Form component
@@ -7,6 +13,7 @@ import lordsEye from "../assets/images/lordsEye.jpg";
 
 //import material ui
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import CssBaseline from '@mui/material/CssBaseline';
 import Card from '@mui/material/Card';
@@ -17,15 +24,17 @@ import CardContent from '@mui/material/CardContent';
 
 const Circuits = () => {
     return (
-        <Container >
+        <Container maxWidth="xl">
             <CssBaseline />
-            <Grid sx={{margin: 1 }}>
+            <Grid container spacing={1} sx={{margin: 1 }}>
+                <Grid item xs={6} sx={{ padding: 1 }}>
                 <Card sx={{ maxWidth: 700 }}>
                     <CardMedia
                         sx={{ height: 500 }}
                         image={lordsEye}
                         title="Lord's Eye"
                     />
+                    <CardContent sx={{ height: 200}}>
                         <Typography sx={{pt: 1 }} gutterBottom variant="h5" component="div">
                         Lord's Eye
                         </Typography>
@@ -33,7 +42,64 @@ const Circuits = () => {
                             This is the most popular CCT route. Depending on the weather and time of day you may come up the hill
                             to see the Lord's Eye shining his light upon you!
                         </Typography>
+                    </CardContent>
                 </Card>
+                </Grid>
+                <Grid item xs={6}>
+                <Card sx={{ maxWidth: 700 }}>
+                    <CardMedia
+                        sx={{ height: 500 }}
+                        image={endoz}
+                        title="Endoz"
+                    />
+                    <CardContent sx={{ height: 200 }}>
+                        <Typography sx={{pt: 1 }} gutterBottom variant="h5" component="div">
+                        Endoz
+                        </Typography>
+                        <Typography sx={{padding: 1 }}variant="body2" color="text.secondary">
+                            This is Loop was founded by Bruster and takes you through the Experimental Farm before you make your way 
+                            to the Endoz drop! The very first time the CCT trotted through this loop Lynch miscalculated his speed going into
+                            a turn and almost bailed. Luckily he lived to tell the tale!
+                        </Typography>
+                    </CardContent>
+                </Card>
+                </Grid>
+                <Grid item xs={6}>
+                <Card sx={{ maxWidth: 700 }}>
+                    <CardMedia
+                        sx={{ height: 500 }}
+                        image={kennysLoop}
+                        title="Kenny's Loop"
+                    />
+                    <CardContent sx={{ height: 200 }}>
+                        <Typography sx={{pt: 1 }} gutterBottom variant="h5" component="div">
+                        Kenny's Loop
+                        </Typography>
+                        <Typography sx={{padding: 1 }}variant="body2" color="text.secondary">
+                            This Loop was founded by Lynch after stopping at our friend Kenny's place for some quick refreshments! It takes you past Ruins Wave where
+                            you make your way around to Gatineau Park before you drop back down to the water fountain.  
+                        </Typography>
+                    </CardContent>
+                </Card>
+                </Grid>
+                <Grid item xs={6}>
+                <Card sx={{ maxWidth: 700 }}>
+                    <CardMedia
+                        sx={{ height: 500 }}
+                        image={champlainLookout}
+                        title="Champlain Lookout"
+                    />
+                    <CardContent sx={{ height: 200 }}>
+                        <Typography sx={{pt: 1 }} gutterBottom variant="h5" component="div">
+                        Champlain Lookout
+                        </Typography>
+                        <Typography sx={{padding: 1 }}variant="body2" color="text.secondary">
+                            This is the most intense ride that the CCT members have done. This climb is required by anyone who wants to join the CCT. Make sure have you lots 
+                            of water and snacks becuase once you get past Pink Lake Lookout you are already too deep into the climb and there is no turning back!
+                        </Typography>
+                    </CardContent>
+                </Card>
+                </Grid>
             </Grid>
 
         </Container>
