ff35bc5efd813204f27b7c3e0b6a715dff6eef58
Marty <martynolan17@gmail.com>
2023-01-17 09:50:22 -0500

    trying to fix app appearance when deployed to heroku

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -6,11 +6,11 @@ import { Link } from 'react-router-dom';
 // import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CssBaseline from '@mui/material/CssBaseline';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
+// import Collapse from '@mui/material/Collapse';
+// import IconButton from '@mui/material/IconButton';
+// import CardActions from '@mui/material/CardActions';
 
 //import Font Awesome icons
 import { FaRegComment } from "react-icons/fa";
