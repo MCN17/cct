786b1b735e1249b8e4d200317d3f414363d3cee0
Marty <martynolan17@gmail.com>
2023-01-17 15:02:46 -0500

    more styling to grid items

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -15,7 +15,7 @@ import FriendList from '../components/FriendList';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
+// import CssBaseline from '@mui/material/CssBaseline';
 
 // import css
 import "./homeStyle.css";
@@ -80,16 +80,16 @@ const Home = () => {
     
 
     return (
-      <Container className="container" maxWidth="xl">
+      <Container className="container">
        
-        <CssBaseline />
+        {/* <CssBaseline /> */}
           {/* <Box sx={{ marginTop: 10, padding: 7, bgcolor: "#eeeeee", borderRadius: "0.3rem" }}> */}
-            <Grid className="homeContainer" container spacing={1}>
+            <Grid className="homeContainer" container spacing={+1}>
               <Grid className="topGrid">
-                <Grid className="imageContainer" item xs={4} sx={{ }}>
+                <Grid className="imageContainer" item xs={+4} sx={{ }}>
                 <img className="cctLogoLarge" src={cctlogo1} alt="outline of horse with parliament buildings in background"></img>
                 </Grid>
-                <Grid className="introContainer" item xs={12}>
+                <Grid className="introContainer" item xs={+12}>
                     <p>Welcome to Cap City Trotters! The CCT was created in 2019 by KMAC, Lynch, Bruster, and Damarts. Our Routes include several loops in both Ottawa and Gatineau.
                     We are always looking for new members so if you want to join you can take a look at some of our main routes and signup if you like what you see!</p>
                     {/* <a href="https://www.strava.com/oauth/authorize?client_id=97775&redirect_uri=http://localhost:3000/home&response_type=code&scope=read_all,activity:read_all">Test Connect to Strava</a> */}
@@ -103,7 +103,7 @@ const Home = () => {
                     <PostForm />
                   </div>
                 )} */}
-              <Grid item xs={6} sx={{ bgcolor: ""}} className="recentPosts">
+              <Grid item xs={+6} sx={{ bgcolor: ""}} className="recentPosts">
                   {loading ? (
                     <div>Loading...</div>
                   ) : (
